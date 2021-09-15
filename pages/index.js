dd5b13053b70b5b3d24c1f40ae754a77e0ad8ba8
Ju Geonjeong <geun0j@gmail.com>
2021-09-15 19:42:30 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppLayout from "../components/AppLayout";
+import 'antd/dist/antd.css'
+
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>Hello, Next!!</div>
+        </AppLayout>
+    );
+}
+
+export default Home;
